4ee2994e71f13d2dc39987f2ddcf96ddf2dfffd2
Prateek Singh <prateek.singh2174@gmail.com>
2025-08-02 12:25:32 -0400

    Added struct pointers for Device, Host, In, Out

--- usb/Core/Inc/usb.h
+++ usb/Core/Inc/usb.h
@@ -8,12 +8,28 @@
 #ifndef INC_USB_H_
 #define INC_USB_H_
 
-void core_init(void);
-void host_init(void);
-void device_init(void);
-bool current_mode(void);
+#include <stdbool.h>
+
+#define USB_OTG_DEVICE ((USB_OTG_DeviceTypeDef *)(USB_OTG_FS_PERIPH_BASE + USB_OTG_DEVICE_BASE))
+#define USB_OTG_IN     ((USB_OTG_INEndpointTypeDef *)(USB_OTG_FS_PERIPH_BASE + USB_OTG_IN_ENDPOINT_BASE))
+#define USB_OTG_OUT    ((USB_OTG_OUTEndpointTypeDef *)(USB_OTG_FS_PERIPH_BASE + USB_OTG_OUT_ENDPOINT_BASE))
+#define USB_OTG_HOST   ((USB_OTG_HostTypeDef *)(USB_OTG_FS_PERIPH_BASE + USB_OTG_HOST_BASE))
+
+void usb_core_init(void);
+void usb_host_init(void);
+void usb_device_init(void);
+bool usb_current_mode(void);
 void USB_Init(void);
 void USB_LP_IRQHandler(void);
+
+void usb_endpoint_init_on_enum(void);
+void usb_endpoint_init_on_reset(void);
+void usb_endpoint_init_on_setaddress(uint8_t addr);
+void usb_endpoint_init_on_setconfig(void);
+void usb_endpoint_init_on_setinterface(void);
+void usb_endpoint_activate(void);
+void usb_endpoint_deactivate(void);
+void usb_packet_read(void);
 
 
 #endif /* INC_USB_H_ */
--- usb/Core/Src/usb.c
+++ usb/Core/Src/usb.c
@@ -6,8 +6,14 @@
  */
 #include "stm32f4xx.h"
 #include "stm32f446xx.h"
+#include "usb.h"
+#include <stdbool.h>
 
-void core_init(void){
+#define USB_OTG_DAINTMSK_IEPM(bit) (1U << (bit))
+#define USB_OTG_DAINTMSK_OEPM(bit) (1U << (16 + bit))
+
+
+void usb_core_init(void){
 
 	USB_OTG_FS->GAHBCFG |= USB_OTG_GAHBCFG_GINT; //Unmask core interrupts
 	USB_OTG_FS->GINTMSK |= USB_OTG_GINTMSK_RXFLVLM; //Enable Rx FIFO non-empty
@@ -36,27 +42,27 @@ void core_init(void){
 
 }
 
-void host_init(void){
+void usb_host_init(void){
 
 	USB_OTG_FS->GINTMSK |= USB_OTG_GINTMSK_PRTIM;
 
-	USB_OTG_FS->HCFG &= ~USB_OTG_HCFG_FSLSPCS;
-	USB_OTG_FS->HCFG |= USB_OTG_HCFG_FSLSPCS_0;
+	USB_OTG_HOST->HCFG &= ~USB_OTG_HCFG_FSLSPCS;
+	USB_OTG_HOST->HCFG |= USB_OTG_HCFG_FSLSPCS_0;
 
 	//3. Program the PPWR bit in OTG_HPRT to 1. This drives VBUS on the USB.
-	USB_OTG_FS->HPRT |= USB_OTG_HPRT_PPWR;
+	USB_OTG_HOST->HPRT |= USB_OTG_HPRT_PPWR;
 	//4. Wait for the PCDET interrupt in OTG_HPRT0. This indicates that a device is connecting to the port.
-	while(!(USB_OTG_FS->HPRT & USB_OTG_HPRT_PENCHNG));
+	while(!(USB_OTG_HOST->HPRT & USB_OTG_HPRT_PENCHNG));
 	//5. Program the PRST bit in OTG_HPRT to 1. This starts the reset process.
-	USB_OTG_FS->HPRT |= USB_OTG_HPRT_PRST;
+	USB_OTG_HOST->HPRT |= USB_OTG_HPRT_PRST;
 	//6. Wait at least 10 ms for the reset process to complete.
 
 	//7. Program the PRST bit in OTG_HPRT to 0.
-	USB_OTG_FS->HPRT &= ~USB_OTG_HPRT_PRST;
+	USB_OTG_HOST->HPRT &= ~USB_OTG_HPRT_PRST;
 	//8. Wait for the PENCHNG interrupt in OTG_HPRT.
-	while(!(USB_OTG_FS->HPRT & USB_OTG_HPRT_PENCHNG));
+	while(!(USB_OTG_HOST->HPRT & USB_OTG_HPRT_PENCHNG));
 	//9. Read the PSPD bit in OTG_HPRT to get the enumerated speed.
-	uint8_t port_speed = (USB_OTG_FS->HPRT & USB_OTG_HPRT_PSPD);
+	uint8_t port_speed = (USB_OTG_HOST->HPRT & USB_OTG_HPRT_PSPD);
 	//10. Program the HFIR register with a value corresponding to the selected PHY clock 1
 
 	//11. Program the FSLSPCS field in the OTG_HCFG register following the speed of the
@@ -68,17 +74,17 @@ void host_init(void){
 	//
 }
 
-void device_init(void){
+void usb_device_init(void){
 
-	USB_OTG_FS->DCFG |= USB_OTG_DCFG_DSPD;
-	USB_OTG_FS->DCFG |= USB_OTG_DCFG_NZLSOHSK;
-	USB_OTG_FS->DCFG &= ~USB_OTG_DCFG_PFIVL;
+	USB_OTG_DEVICE->DCFG |= USB_OTG_DCFG_DSPD;
+	USB_OTG_DEVICE->DCFG |= USB_OTG_DCFG_NZLSOHSK;
+	USB_OTG_DEVICE->DCFG &= ~USB_OTG_DCFG_PFIVL;
 
 	//2. Program the Device threshold control register. This is required only if you are using
 	//DMA mode and you are planning to enable thresholding.
 
 	//3. Clear the DCTL.SDIS bit. The core issues a connect after this bit is cleared.
-	USB_OTG_FS->DCTL &= ~USB_OTG_DCTL_SDIS;
+	USB_OTG_DEVICE->DCTL &= ~USB_OTG_DCTL_SDIS;
 
 	USB_OTG_FS->GINTMSK |= USB_OTG_GINTMSK_USBRST;
 	USB_OTG_FS->GINTMSK |= USB_OTG_GINTMSK_ENUMDNEM;
@@ -95,12 +101,76 @@ void device_init(void){
 //	read the OTG_DSTS
 //	register to determine the enumeration speed and perform the steps listed in Endpoint
 //	initialization on enumeration completion on page 1226.
-	uint8_t enum_speed = (USB_OTG_FS->DSTS & USB_OTG_DSTS_ENUMSPD);
+	uint8_t enum_speed = (USB_OTG_DEVICE->DSTS & USB_OTG_DSTS_ENUMSPD) >> 1;
 }
 
-bool current_mode(void){
+bool usb_current_mode(void){
 
 	return (USB_OTG_FS->GINTSTS & USB_OTG_GINTSTS_CMOD);
+}
+
+void usb_endpoint_init_on_enum(void){
+
+	//Called when OTG_GINTSTS_ENUMDNE = 1
+	uint8_t enum_speed = (USB_OTG_DEVICE->DSTS & USB_OTG_DSTS_ENUMSPD) >> 1;
+
+	USB_OTG_IN->DIEPCTL[0] &= ~USB_OTG_DIEPCTL_MPSIZ;
+	USB_OTG_IN->DIEPCTL[0] |= (USB_OTG_DIEPCTL_MPSIZ & enum_speed);
+
+}
+
+void usb_endpoint_init_on_reset(void){
+
+	//1. Set the NAK bit for all OUT endpoints
+
+	//2. Unmask the following interrupt bits
+	USB_OTG_DEVICE->DAINTMSK |= USB_OTG_DAINTMSK_IEPM(0);
+	USB_OTG_DEVICE->DAINTMSK |= USB_OTG_DAINTMSK_OEPM(0);
+	USB_OTG_DEVICE->DOEPMSK |= USB_OTG_DOEPMSK_STUPM;
+	USB_OTG_DEVICE->DOEPMSK |= USB_OTG_DOEPMSK_XFRCM;
+	USB_OTG_DEVICE->DIEPMSK |= USB_OTG_DIEPMSK_XFRCM;
+	USB_OTG_DEVICE->DIEPMSK |= USB_OTG_DIEPMSK_TOM;
+
+	//3. Set up the data FIFO RAM for each of the FIFOs
+
+	//4. Program the following fields in the endpoint-specific registers for control OUT endpoint
+	//0 to receive a SETUP packet
+
+}
+
+void usb_endpoint_init_on_setaddress(uint8_t addr){
+
+	//1. Program the OTG_DCFG register with the device address received in the SetAddress command
+	USB_OTG_DEVICE->DCFG &= ~USB_OTG_DCFG_DAD;
+	addr &= USB_OTG_DCFG_DAD_Msk;
+	USB_OTG_DEVICE->DCFG |= (addr << USB_OTG_DCFG_DAD_Pos) & USB_OTG_DCFG_DAD;
+
+	//2. Program the core to send out a status IN packet
+}
+
+void usb_endpoint_init_on_setconfig(void){
+
+
+}
+
+void usb_endpoint_init_on_setinterface(void){
+
+
+}
+
+void usb_endpoint_activate(void){
+
+
+}
+
+void usb_endpoint_deactivate(void){
+
+
+}
+
+void usb_packet_read(void){
+
+
 }
 
 void USB_Init(void){
